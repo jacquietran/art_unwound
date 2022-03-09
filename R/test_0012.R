c9cc02b6be0d4e70492c48fe4004e35b2f0b64ae
Jacquie Tran <jacquie.tran@gmail.com>
2022-03-10 00:44:48 +1300

    More tests

---
+++ R/test_0012.R
@@ -0,0 +1,60 @@
+# Load libraries ---------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+library(ggforce)
+
+# Set parameters ---------------------------------------------------------------
+
+iteration_id <- "test_0012"
+initial_seed <- 114262
+num_groups <- 100
+colour_vec <- c(
+  "#DBEDFF", "#B6DAFF", "#A4D1FF", "#91C7FF", "#7FBEFF", "#6CB4FF", "#47A0FF", "#FFFFFF")
+
+# Create data ------------------------------------------------------------------
+
+num_points <- num_groups * 4
+
+set.seed(initial_seed)
+seed_vec <- sample(seq(1, 100000, by = 1), 2, replace = FALSE)
+
+set.seed(seed_vec[1])
+x_weights <- scales::rescale(
+  sample(seq(1, 500000, by = 1), 10, replace = TRUE))
+
+set.seed(seed_vec[2])
+y_weights <- scales::rescale(
+  sample(seq(1, 500000, by = 1), 10, replace = TRUE))
+
+integer_vec <- seq(1, 10, by = 1)
+
+outline_vec <- c(colour_vec, NA, "#000000")
+
+set.seed(initial_seed)
+data <- tibble::tibble(
+  x = sample(integer_vec, num_points, replace = TRUE, prob = x_weights),
+  y = rep(
+    sample(c(1,10), num_points / 2, replace = TRUE), each = 2),
+  group = rep(seq(1, num_groups, by = 1), each = 4),
+  hex_fill = sample(colour_vec, num_points, replace = TRUE),
+  hex_outline = sample(outline_vec, num_points, replace = TRUE))
+
+# Build plot -------------------------------------------------------------------
+
+p <- ggplot() +
+  geom_diagonal_wide(
+    data = data,
+    aes(x = x, y = y, group = group, fill = hex_fill),
+    strength = 0.75, colour = NA, size = 0, alpha = 1) +
+  scale_fill_identity() +
+  # scale_colour_identity() +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "#FFFFFF", colour = "#FFFFFF"))
+
+# Export to file ---------------------------------------------------------------
+
+ggsave(
+  here::here(glue::glue("img/{`iteration_id`}.png")),
+  last_plot(), width = 10, height = 10, units = "cm", dpi = 300)
